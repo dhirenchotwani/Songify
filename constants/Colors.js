164b79ce5d44b7a5cdf4868afeb9c390b6945c79
dhirenchotwani <dhirenchotwani@gmail.com>
2019-06-27 10:14:44 +0530

    Added initial Update

---
+++ constants/Colors.js
@@ -0,0 +1,25 @@
+const tintColor = '#2f95dc';
+
+export default {
+  tintColor,
+  tabIconDefault: '#ccc',
+  tabIconSelected: tintColor,
+  tabBar: '#fefefe',
+  errorBackground: 'red',
+  errorText: '#fff',
+  warningBackground: '#EAEB5E',
+  warningText: '#666804',
+  noticeBackground: tintColor,
+  noticeText: '#fff',
+
+  //Our Colors
+  primaryColor:'#19182F',
+  accentColor:'#',
+  placeholderColor:'#',
+  accentGradientStart:'#',
+  accentGradientEnd:'#',
+  PrimaryGradientStart:'#',
+  PrimaryGradientEnd:'#',
+  headingColor:'#',
+
+};
